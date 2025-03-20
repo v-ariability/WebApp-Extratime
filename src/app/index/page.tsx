2a350b56ef3b67e6eea13fcc0d434dc0ed0ed525
Milanjiji <milanjiji0987654321@gmail.com>
2025-03-20 22:49:45 +0530

    hero section

--- src/app/index/page.tsx
+++ src/app/index/page.tsx
@@ -1,12 +1,18 @@
 'use client'
 import React,{useContext} from 'react'
 import {AppContext} from '@/context/wrapper'
+import Header from '@/components/Index/Header'
+import Hero from '@/components/Index/Hero'
+
 
 const Index = () => {
 
     const {sample} = useContext(AppContext)
   return (
-    <div>{sample}</div>
+    <div>
+        <Header />
+        <Hero />
+    </div>
   )
 }
 
