9357590de7c997efd0aa2491d2210330b998cf41
Milanjiji <milanjiji0987654321@gmail.com>
2025-03-21 23:12:02 +0530

    till payments

---
+++ components/Index/Faculties.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Faculties = () => {
+  return (
+    <div className='mt-[98px] mx-[28px]' >
+        <div className='flex items-cente ' >
+            <p className='w-[48px] h-[48px] flex justify-center items-center bg-[#040404] !text-[var(--background)] rounded-full' >3</p>        
+            <p className='px-[24px] py-[12px] rounded-[28px] bg-[var(--subwhite)]' >FACILITIES</p>        
+        </div>
+
+        <div className='mt-[48px]' >
+            <p className='text-[56px] font-[500] text-center' >Explore our facilities</p>
+            <p className='text-[16px] font-[400] text-center mt-[24px]' >From high-quality turfs and professional-grade courts to fully-equipped arenas, our facilities are designed for every athlete. Whether <br /> you're playing for fun or competing at the highest level, experience top-tier sports environments that enhance your game. Find the perfect <br /> venue and play without limits</p>
+        </div>
+
+        <div className='bg-[var(--subwhite)] rounded-[18px] py-[24px] px-[30px] mt-[24px]' >
+            <div className='flex justify-end items-center gap-[12px]' >
+                <input 
+                    className='bg-white rounded-[28px] py-[13px] px-[24px] w-[460px]'
+                    placeholder='Enter your pincode '
+                    type="text" /> 
+                <button className='px-[52px] py-[12px] rounded-[28px] text-[18px] font-[500] bg-[var(--orange)] !text-[var(--background)]' >View all</button>
+            </div>
+            <Image src={'/index/GoogleMapsWidget.png'} className='!w-[100%] mt-[32px]' width={1000} height={1500} alt='map' />
+            <p className='text-[16px] text-center font-[400] mt-[32px]' >Find top sports facilities near you with ease! Whether it's a turf, court, or arena, locate the perfect venue for your <br /> next game in just a few taps. Play at the best spots, anytime, anywhere</p>
+
+            <div className='flex justify-between gap-[24px] mt-[24px]' >
+
+                <div className='w-[calc((100vw-142px)/4)] h-[calc((100vw-142px)/4)]  p-[12px] rounded-[18px] flex justify-start items-end align-bottom bg-[url("/index/Rectangle41.png")] bg-cover' >
+                    <p className='!text-[var(--background)] text-[18px] font-[400] leading-[24px]' >Professionally <br /> Designed Courts</p>
+                </div>
+                <div className='w-[calc((100vw-142px)/4)] h-[calc((100vw-142px)/4)]  p-[12px] rounded-[18px] flex justify-start items-end align-bottom bg-[url("/index/Rectangle42.png")] bg-cover' >
+                    <p className='!text-[var(--background)] text-[18px] font-[400] leading-[24px]' >State- <br />of-the-Art Lighting</p>
+                </div>
+                <div className='w-[calc((100vw-142px)/4)] h-[calc((100vw-142px)/4)]  p-[12px] rounded-[18px] flex justify-start items-end align-bottom bg-[url("/index/Rectangle43.png")] bg-cover' >
+                    <p className='!text-[var(--background)] text-[18px] font-[400] leading-[24px]' >Safe & <br />Well-Maintained Surfaces</p>
+                </div>
+                <div className='w-[calc((100vw-142px)/4)] h-[calc((100vw-142px)/4)]  p-[12px] rounded-[18px] flex justify-start items-end align-bottom bg-[url("/index/Rectangle44.png")] bg-cover' >
+                    <p className='!text-[var(--background)] text-[18px] font-[400] leading-[24px]' >Multi- <br />Sport Compatibility</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Faculties
---
+++ components/Index/Payments.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Payments = () => {
+  return (
+    <div className='mt-[98px] px-[28px]' >
+         <div className='flex items-center ' >
+            <p className='w-[48px] h-[48px] flex justify-center items-center bg-[#040404] !text-[var(--background)] rounded-full' >4</p>        
+            <p className='px-[24px] py-[12px] rounded-[28px] bg-[var(--subwhite)]' >PAYMENTS & REFUNDS</p>        
+        </div>
+
+        <div className='flex flex-col items-center' >
+          <p className='text-[56px] font-[500] leading-[64px] text-center mt-[48px]' >Seamless payments & <br /> Hassle-Free refunds.</p>
+          <p className='text-[18px] font-[400] text-center mt-[24px]' >Focus on the game, not the transaction! Enjoy secure, fast, and hassle-free payments with multiple options, including UPI, <br /> cards, and wallets. Need to cancel? Our smooth refund process ensures quick and easy reimbursements, so you <br /> can book with confidence every time</p>
+
+          <ul className='flex mt-[24px] ' >
+            <li><p className='px-[18px] py-[4px] font-[500] text-[14px] rounded-[48px] bg-[var(--subwhite)]' >UPI Payment</p></li>
+            <li><p className='px-[18px] py-[4px] font-[500] text-[14px] rounded-[48px] bg-[var(--subwhite)]' >Credit Card</p></li>
+            <li><p className='px-[18px] py-[4px] font-[500] text-[14px] rounded-[48px] bg-[var(--orange)] !text-white' >In App Credit</p></li>
+          </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Payments
